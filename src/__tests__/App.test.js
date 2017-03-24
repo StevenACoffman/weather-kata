be91e61c82fd642d280eaaf715c897d34a533857
Matthew Morgan <matt@dulcetsoftware.com>
2017-03-24 17:20:37 -0400

    Example of inline mock

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -7,6 +7,8 @@ import renderer from 'react-test-renderer';
 import * as mockedWeather from '../__mocks__/weather';
 
 
+
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
@@ -29,3 +31,21 @@ it('can inject the mock into the App component', () => {
   tree = component.toJSON();
   expect(tree).toMatchSnapshot(); //second snapshot for this test
 });
+
+it('uses the other mock syntax', () => {
+  const makeCallAsync = jest.fn();
+  makeCallAsync.mockReturnValue(new Promise((resolve) => resolve({name: 'South Lyon'})));
+
+  const otherWeather = { makeCallAsync };
+
+  const div = document.createElement('div');
+  const component = renderer.create(<App weather={otherWeather}/>, div);
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot(); //first snapshot for this test
+
+  tree.children[2].props.onClick(); //manually trigger the button, which is nested
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot(); //second snapshot for this test
+
+});
+
